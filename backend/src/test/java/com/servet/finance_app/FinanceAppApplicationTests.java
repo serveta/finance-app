a6c53b0efe3216408bd4bc6719f7feff40801e05
Servet Arslan <arslannservett@gmail.com>
2025-01-24 13:12:55 +0300

    initial commit

---
+++ backend/src/test/java/com/servet/finance_app/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servet.finance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
